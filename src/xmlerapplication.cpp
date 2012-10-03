62d01c4d991238977e7f012ebf1cef6853abbbbb
elemc <me@elemc.name>
2012-10-04 02:41:42 +0400

    Forgot edit theme name in application class

--- src/xmlerapplication.cpp
+++ src/xmlerapplication.cpp
@@ -58,7 +58,7 @@ void XMLerApplication::openFiles ()
 void XMLerApplication::selectIconTheme ()
 {
   QString generic_icon_to_check = "code-context";
-  QString fallback_icon_theme = "oxygen-int";
+  QString fallback_icon_theme = "oxygen-internal";
 
   if ( !QIcon::hasThemeIcon( generic_icon_to_check ) )
     QIcon::setThemeName( fallback_icon_theme );
