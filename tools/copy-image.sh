0a200f619df775b37ae17aa372ab47822845eb99
elemc <me@elemc.name>
2012-10-30 15:49:38 +0400

    Add script for copy images from oxygen theme

---
+++ tools/copy-image.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+names=$*
+folders="16x16 22x22 32x32 48x48"
+base_dir="/usr/share/icons/oxygen"
+part_dir="actions"
+
+rcc_add=""
+
+for name in $names; do
+    for size in $folders; do
+        #cp -R ${base_dir}/${size}/${part_dir}/${name}.png icons/${size}/
+        rcc_add="$rcc_add\n<file alias=\"${size}/${name}.png\">icons/${size}/${name}.png</file>"
+    done
+done
+echo -e $rcc_add
